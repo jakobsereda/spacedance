7b11b497fc4ced9241be30a6b8d9298f1832b8b4
Re-Emzee <molave.liggayu@gmail.com>
2024-10-06 11:51:45 -0700

    gameMenu buttons

--- app/src/main/java/org/example/ui/GameMenu.java
+++ app/src/main/java/org/example/ui/GameMenu.java
@@ -1,20 +1,38 @@
 package org.example.ui;
+import org.example.Timer;
 
 import javax.swing.*;
 
 import org.example.model.Difficulty;
 import org.example.model.Game;
 
-public class GameMenu extends JPanel {
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class GameMenu extends JPanel implements ActionListener {
+    private MainFrame parent;
     private JButton quitButton;
     private Game game;
+    private Timer time;
 
     public GameMenu(Difficulty difficulty, int numPositions) {
         game = new Game(difficulty,numPositions);
+        time = new Timer();
+    }
+
+    public void initButtons() {
+        Color buttonColor = new Color(White)
+        quitButton = new JButton(" Quit ");
+        initButton(quitButton, buttonColor, 300, 80, 20);
     }
 
     //TODO method to display image
+    public void displayImage(){
 
+    }
     //TODO display timer
+    public void displayTimer(){
+
+    }
     //TODO quit game
 }
