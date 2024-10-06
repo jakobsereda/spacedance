33551a956a5d5068ce4f47a752ffae4622bbc637
jacob <jacobmkpark@gmail.com>
2024-10-06 11:13:27 -0700

    GameMenu

--- app/src/main/java/org/example/ui/GameMenu.java
+++ app/src/main/java/org/example/ui/GameMenu.java
@@ -3,11 +3,18 @@ package org.example.ui;
 import javax.swing.*;
 
 import org.example.model.Difficulty;
+import org.example.model.Game;
 
 public class GameMenu extends JPanel {
+    private JButton quitButton;
+    private Game game;
 
-    public GameMenu(Difficulty difficulty) {
-
+    public GameMenu(Difficulty difficulty, int numPositions) {
+        game = new Game(difficulty,numPositions);
     }
-    
+
+    //TODO method to display image
+
+    //TODO display timer
+    //TODO quit game
 }
