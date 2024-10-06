6ce45ee94b34d44507ea530e079cb4aef86d0414
jakob <138621739+jakobsereda@users.noreply.github.com>
2024-10-05 22:33:15 -0700

    feat: simple MainFrame

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,10 @@
  */
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.example.ui.MainFrame;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        new MainFrame();
     }
 }
---
+++ app/src/main/java/org/example/ui/MainFrame.java
@@ -0,0 +1,12 @@
+package org.example.ui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        setTitle("My Main Frame");
+        setSize(400, 300); 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        setVisible(true);
+    }
+}
